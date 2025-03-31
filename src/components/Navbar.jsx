0ba10b42f791814bb23c32a2533342494c858f5c
Vadym <149205033+XXXDoriXXX@users.noreply.github.com>
2025-03-31 11:33:57 +0300

    navbar fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,22 +6,22 @@ import rightBg from "../assets/rightNavbar.svg";
 
 const Navbar = () => {
     return (
-        <div className="navbar-container">
-            <img src={leftBg} alt="left" className="navbar-left-bg" />
-            <img src={rightBg} alt="right" className="navbar-right-bg" />
+        <div className="navbar">
+            <div className="navbar-left-block">
+                <img src={leftBg} alt="left" className="navbar-bg" />
+                <img src={logo} alt="logo" className="navbar-logo-img" />
+                <span className="navbar-title">Military nets</span>
+            </div>
 
-            <nav className="navbar">
-                <div className="navbar-logo">
-                    <img src={logo} alt="logo" />
-                    <span>Military nets</span>
-                </div>
+            <div className="navbar-right-block">
+                <img src={rightBg} alt="right" className="navbar-bg" />
                 <ul className="navbar-links">
                     <li><a href="#">Home</a></li>
                     <li><a href="#">Other</a></li>
                     <li><a href="#">Name</a></li>
                 </ul>
-                <div className="navbar-button" />
-            </nav>
+                <div className="navbar-button"></div>
+            </div>
         </div>
     );
 };
