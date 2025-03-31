576c826966aa3b9b8b25c990434aba767a3a0201
tolia k <tolia.kohan@gmail.com>
2025-03-31 13:04:11 +0300

    add login

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -3,11 +3,13 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import MainPage from "../pages/MainPage.jsx";
+import Login from "../pages/Login.jsx";
 
 const AppRouter = () => (
     <BrowserRouter>
         <Routes>
             <Route path="/" element={<MainPage />} />
+            <Route path="/login" element={<Login />} />
         </Routes>
     </BrowserRouter>
 );
