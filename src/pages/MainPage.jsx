85ec9451ededc62261f5b01f746b124918b21b02
Dehimik <vladkepkin@gmail.com>
2025-03-31 10:29:29 +0300

    Navbar v1

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -2,6 +2,8 @@ import React from "react";
 
 import { useEffect, useState } from "react";
 import ScheduleTable from "../components/ScheduleTable";
+import {Navbar} from "../components/navbar";
+import { Link } from "react-router-dom";
 
 const MainPage = () => {
     const [schedule, setSchedule] = useState([]);
@@ -15,11 +17,14 @@ const MainPage = () => {
     }, []);
 
     return (
-        <div className="p-4">
-            <h1 className="text-2xl font-bold mb-4">Привіт!</h1>
-            <h2 className="text-xl font-semibold mb-2">Розклад сіток</h2>
-            <ScheduleTable data={schedule} />
-        </div>
+        <>
+            <Navbar/>
+            <div className="p-4">
+                <h1 className="text-2xl font-bold mb-4">Привіт!</h1>
+                <h2 className="text-xl font-semibold mb-2">Розклад сіток</h2>
+                <ScheduleTable data={schedule} />
+            </div>
+        </>
     );
 };
 
